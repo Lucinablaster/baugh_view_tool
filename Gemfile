d319d258877eb2ad0d6f22779f5dc8201c7b705d
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 15:09:45 +0000

    Dev tool initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in baugh_view_tool.gemspec
+gemspec
---
+++ lib/baugh_view_tool.rb
@@ -0,0 +1,6 @@
+require "baugh_view_tool/version"
+require "baugh_view_tool/renderer"
+
+module BaughViewTool
+  # Your code goes here...
+end
